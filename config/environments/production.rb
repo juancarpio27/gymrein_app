9e76325c03ba19edbd125414f95bbd29fbc53cd3
juancarpio27 <juan.carpio01@gmail.com>
2017-02-20 18:44:26 -0600

    Image path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
@@ -47,7 +47,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -76,7 +76,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
@@ -89,7 +89,7 @@ Rails.application.configure do
   #config.action_mailer.perform_deliveries = true
   #config.action_mailer.raise_delivery_errors = false
   #config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_url_options = { :host => 'gymrein.herokuapp.com' }
+  config.action_mailer.default_url_options = {:host => 'gymrein.herokuapp.com'}
 
   config.action_mailer.smtp_settings = {
       :user_name => ENV["EMAIL"],
@@ -104,11 +104,14 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
       storage: :s3,
+      :s3_host_name => 's3-us-west-2.amazonaws.com',
       s3_credentials: {
           bucket: ENV.fetch('S3_BUCKET_NAME'),
           access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
           secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
           s3_region: ENV.fetch('AWS_REGION'),
-      }
+      },
+      :url => ':s3_domain_url',
+      :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
 end
