0972b3fa0b515b4c55155333040749b6f6148709
juancarpio27 <juan.carpio01@gmail.com>
2017-03-12 18:43:29 -0600

    reservation and waiting list

--- db/migrate/20170313003450_add_columns_to_waiting_list.rb
+++ db/migrate/20170313003450_add_columns_to_waiting_list.rb
@@ -3,6 +3,4 @@ class AddColumnsToWaitingList < ActiveRecord::Migration[5.0]
     add_column :waiting_lists, :user_id, :integer
     add_column :waiting_lists, :class_date_id, :integer
   end
-  add_foreign_key :waiting_lists, :users
-  add_foreign_key :waiting_lists, :class_dates
 end
