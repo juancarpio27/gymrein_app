2c96a162da5436e65cd7f7bdc438a7d5d354604c
juancarpio27 <juan.carpio01@gmail.com>
2017-05-03 11:54:31 -0500

    pushmeup

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -22,4 +22,4 @@ end
 APNS.port = 2195
 # this is also the default. Shouldn't ever have to set this, but just in case Apple goes crazy, you can.
 
-APNS.pem = ENV['IOS_CERTIFICATE_PATH']
+APNS.pem = File.read(Rails.root.join('config','MyGymCKUnEn.pem'))
