7d7b4e79249b548b6e55b8fbaaae4b415c525067
juancarpio27 <juan.carpio01@gmail.com>
2017-05-03 19:24:32 -0500

    security to locations

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::LocationsController < Api::ApiController
-
-  before_action :assert_user
-
+  
   def show
     @location = Location.find(params[:id])
     render json: @location.as_json
