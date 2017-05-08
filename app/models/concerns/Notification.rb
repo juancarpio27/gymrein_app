caa762a4fc4d6e1aae910eb73118365e68ab067a
juancarpio27 <juan.carpio01@gmail.com>
2017-05-07 23:29:11 -0500

    android message

--- app/models/concerns/Notification.rb
+++ app/models/concerns/Notification.rb
@@ -18,14 +18,15 @@ class Notification
     connection = Houston::Connection.new(Houston::APPLE_DEVELOPMENT_GATEWAY_URI, certificate, passphrase)
     connection.open
     notification = Houston::Notification.new(device: session.device_id)
-    notification.alert = 'Se desocupo un puesto en la clase ' + class_date.event.name + '. Tu clase empieza a las ' + class_date.date.strftime("%H:%M %m/%d/%y")
+    notification.alert = 'Se desocupó un puesto en la clase de' + class_date.event.name + '. Tu clase empieza a las ' + class_date.date.strftime("%H:%M %m/%d/%y")
     connection.write(notification.message)
     connection.close
   end
 
   def self.send_android_notification(session,class_date)
     destination = [session.device_id]
-    data = {message: 'You have a new class!'}
+    message = 'Se desocupó un puesto en la clase de' + class_date.event.name + '. Tu clase empieza a las ' + class_date.date.strftime("%H:%M %m/%d/%y")
+    data = {message: message}
     GCM.send_notification( destination, data )
   end
 
